51483c48b16374d8d7864ec267534cee245ff767
Stirling Hart <hartstir@ecs.vuw.ac.nz>
2025-03-01 15:28:43 +1300

    Mouse look rotation

--- ship_player.gd
+++ ship_player.gd
@@ -1,5 +1,7 @@
 extends ShipCharacter
 
+# only necessary for mouse look mode
+@export var max_angular_velocity: float = 2.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,7 +14,29 @@ func _process(delta):
 
 
 func _get_input():
-	rotation_input = Input.get_axis("Left", "Right")
-	thrust_input = Input.get_axis("Down", "Up")
+	if Input.is_action_pressed("Shift"):
+		rotation_input = Input.get_axis("Left", "Right")
+		thrust_input = Input.get_axis("Down", "Up")
+	else:
+		# slow down if spinning too fast
+		if abs(angular_velocity) > max_angular_velocity:
+			rotation_input = sign(angular_velocity) * -1
+		else:
+			var angle_to_mouse = global_position.direction_to(get_global_mouse_position()).angle() + PI/2
+			var angle_to_mouse_from_face = fmod((angle_to_mouse - rotation + PI), (2 * PI)) - PI
+			
+			if angle_to_mouse_from_face < 0:
+				rotation_input = -1
+			elif angle_to_mouse_from_face > 0:
+				rotation_input = 1
+			
+			# if within a certain angle range and your angular velocity is aiming for the goal, cap ang vel
+			var threshold = PI/4
+			
+			if abs(angle_to_mouse_from_face) < threshold && sign(angular_velocity) == sign(angle_to_mouse_from_face):
+				#scale between 0 angular velocity at no angle and max at the threshold angle
+				var scaled_max_velocity = abs(angle_to_mouse_from_face) / threshold * max_angular_velocity
+				# Clamp angular velocity while preserving sign
+				angular_velocity = sign(angular_velocity) * min(abs(angular_velocity), scaled_max_velocity)
 
 	brake_active = Input.is_action_pressed("Brake")
